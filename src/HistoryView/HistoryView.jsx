57aae821db563649b7ca37258390a60f81bdb26d
Zons@Milchkanne <j.f.abert@gmail.com>
2019-05-12 12:48:30 +0200

    add fake data

--- src/HistoryView/HistoryView.jsx
+++ src/HistoryView/HistoryView.jsx
@@ -29,7 +29,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export const HistoryView = () => {
+export const HistoryView = ({hostedEvents, visitedEvents}) => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
@@ -48,8 +48,23 @@ export const HistoryView = () => {
           <Tab label="Hosted" className={classes.tab}/>
         </Tabs>
       </AppBar>
-      {value === 0 && <TabContainer><FoodEventList/></TabContainer>}
-      {value === 1 && <TabContainer><FoodEventList/></TabContainer>}
+      {value === 0 && <TabContainer><FoodEventList foodEvents={visitedEvents}/></TabContainer>}
+      {value === 1 && <TabContainer><FoodEventList foodEvents={hostedEvents} /></TabContainer>}
     </div>
   );
+}
+
+HistoryView.defaultProps={
+  hostedEvents: [
+    {title: 'Pizza Funghi', price: 2, tags: ['vegetarian'], userRating: 5, foodRating: 2, image: 'https://cdn.pixabay.com/photo/2017/09/30/15/10/pizza-2802332_960_720.jpg' },
+    {title: 'Brokkoli Soup', price: 0, tags: ['glutenFree'], userRating: 0, foodRating: 2.2, image: 'https://cdn.pixabay.com/photo/2014/12/16/23/45/soup-570922_960_720.jpg'},
+    {title: 'Chocolate Cake', price: 3, tags: ['vegan', 'glutenFree'], userRating: 2.2, foodRating: 5. , image: 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg'},
+    {title: 'Cheese Burger', price: 2, tags: ['vegan'], userRating: 5, foodRating: 2, image: 'https://cdn.pixabay.com/photo/2015/07/01/07/06/burger-827309_960_720.jpg'},
+  ],
+  visitedEvents: [
+    {title: 'Pad Thai', price: 5, tags: ['vegan'], userRating: 5, foodRating: 5, image: 'https://cdn.pixabay.com/photo/2017/06/19/14/15/pad-2419459_960_720.jpg'},
+    {title: 'Maultaschen', price: 2, tags: [''], userRating: 5, foodRating: 2, image: 'https://cdn.pixabay.com/photo/2013/01/08/01/20/broth-74274_960_720.jpg'},
+    {title: 'Hamburger', price: 4, tags: ['vegan'], userRating: 5, foodRating: 4.5, image: 'https://cdn.pixabay.com/photo/2015/07/01/07/06/burger-827310_960_720.jpg'},
+    
+  ]
 }
